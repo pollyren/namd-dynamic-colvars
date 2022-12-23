3e67c6f0696f744bc693d6a10e277ec0b09bbd14
Polly Ren <renpollyf@gmail.com>
2022-12-23 11:50:55 -0800

    added --wait flag in sbatch script

--- consec_colvars_submit.sh
+++ consec_colvars_submit.sh
@@ -12,18 +12,19 @@ last_npt=$((current_npt+total_runs))
 
 for ((i=0; i<$total_runs; i++));
 do
-    create_minmaxtcl=$(sbatch run.sh $prev_npt create_minmaxtcl | cut -f 4 -d' ')
-    create_minmax_sbatch=$(sbatch --dependency=afterok:$create_minmaxtcl run.sh $prev_npt minmax_sbatch | cut -f 4 -d' ')
-    minmax_submit=$(sbatch --dependency=afterok:$create_minmax_sbatch minmax-npt$prev_npt.sh | cut -f 4 -d' ')
+    create_minmaxtcl=$(sbatch --wait run.sh $prev_npt create_minmaxtcl | cut -f 4 -d' ')
+    create_minmax_sbatch=$(sbatch --wait --dependency=afterok:$create_minmaxtcl run.sh $prev_npt minmax_sbatch | cut -f 4 -d' ')
+    minmax_submit=$(sbatch --wait --dependency=afterok:$create_minmax_sbatch minmax-npt$prev_npt.sh | cut -f 4 -d' ')
 
-    create_centretcl=$(sbatch --dependency=afterok:$minmax_submit run.sh $prev_npt create_centretcl | cut -f 4 -d' ')
-    create_centre_sbatch=$(sbatch --dependency=afterok:$create_centretcl run.sh $prev_npt centre_sbatch | cut -f 4 -d' ')
-    centre_submit=$(sbatch --dependency=afterok:$create_centre_sbatch centre-npt$prev_npt.sh | cut -f 4 -d' ')
+    create_centretcl=$(sbatch --wait --dependency=afterok:$minmax_submit run.sh $prev_npt create_centretcl | cut -f 4 -d' ')
+    create_centre_sbatch=$(sbatch --wait --dependency=afterok:$create_centretcl run.sh $prev_npt centre_sbatch | cut -f 4 -d' ')
+    centre_submit=$(sbatch --wait --dependency=afterok:$create_centre_sbatch centre-npt$prev_npt.sh | cut -f 4 -d' ')
 
-    create_colvars=$(sbatch --dependency=afterok:$centre_submit run.sh $current_npt create_colvars $harwall_force | cut -f 4 -d' ')
-    create_conf=$(sbatch --dependency=afterok:$create_colvars run.sh $current_npt create_colvars $npt_steps | cut -f 4 -d' ')
-    create_sh=$(sbatch --dependency=afterok:$create_conf run.sh $current_npt job_submit | cut -f 4 -d' ')
-    job_submit=$(sbatch --dependency=afterok:$create_sh npt$current_npt-consec.sh | cut -f 4 -d' ')
+    create_colvars=$(sbatch --wait --dependency=afterok:$centre_submit run.sh $current_npt create_colvars $harwall_force | cut -f 4 -d' ')
+    create_conf=$(sbatch --wait --dependency=afterok:$create_colvars run.sh $current_npt create_colvars $npt_steps | cut -f 4 -d' ')
+    create_sh=$(sbatch --wait --dependency=afterok:$create_conf run.sh $current_npt job_submit | cut -f 4 -d' ')
+    job_submit=$(sbatch --wait --dependency=afterok:$create_sh npt$current_npt-consec.sh | cut -f 4 -d' ')
 
+    prev_npt=$current_npt
     current_npt=$((current_npt + 1))
 done
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=run python
+#SBATCH --job-name=runpython
 #SBATCH --time=36:00:00
 #SBATCH --exclusive
 #SBATCH --partition=caslake
-#SBATCH --nodes=6
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=48
 #SBATCH --account=pi-haddadian
 
@@ -12,4 +12,4 @@ ARG2=${2:-0}  # function name
 ARG3=${3:-0}  # function param (optional)
 
 module load python
-python consec_colvars.py $1 $2 $3
+python consec_colvars.py $ARG1 $ARG2 $ARG3
