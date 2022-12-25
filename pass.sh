ad3c8c4e8115f0ff4c21f49e55fa58115333a74e
Polly Ren <renpollyf@gmail.com>
2022-12-24 22:13:31 -0800

    removed extraneous .sh files

--- pass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=null
-#SBATCH --time=00:00:01
-#SBATCH --exclusive
-#SBATCH --partition=caslake
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
-#SBATCH --account=pi-haddadian
