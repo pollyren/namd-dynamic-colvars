1034a651104c658d07a94d7aaaa13a665bd08cee
Polly Ren <renpollyf@gmail.com>
2022-12-25 00:13:39 -0800

    updated docstrings in generate_sh.py

--- generate_sh.py
+++ generate_sh.py
@@ -1,19 +1,32 @@
 import sys
 import os
 
-def sbatch_string(job_name):
+def sbatch_string(job_name: str) -> str:
     """
     Generates a string for the sbatch file header.
 
     Inputs:
         job_name (string): name of the job
 
-    Output: a string containing the header of the sbatch file
+    Output: a string containing the header of the sbatch file (string)
     """
     string = "#!/bin/sh\n#SBATCH --job-name={}\n#SBATCH --time=36:00:00\n#SBATCH --exclusive\n#SBATCH --partition=caslake\n#SBATCH --nodes=6\n#SBATCH --ntasks-per-node=48\n#SBATCH --account=pi-haddadian\n\n".format(str(job_name))
     return string
 
-def create_individual_sbatch(job_num, start_npt, distance, total_runs_per_distance, npt_steps, harwall_force):
+def create_individual_sbatch(job_num: int, start_npt: int, distance: int, total_runs_per_distance: int, npt_steps: int, harwall_force: int) -> str:
+    """
+    Creates individual bash submit script for each batch of runs (a single batch contains all runs with the same wall distance)
+
+    Inputs:
+        job_num (int): the job number within the total number of npt runs
+        start_npt (int): the npt at which to begin the current batch
+        distance (int): the wall distance from the minmax of the protein for current batch
+        total_runs_per_distance (int): the number of times to continue current batch
+        npt_steps (int): the number of steps between wall recalculation 
+        harwall_force (int): the force of colvars harmonic wall
+
+    Output: the file name of the bash submit script (string)
+    """
     job_name = "run{}".format(job_num)
     sbatch = sbatch_string(job_name)
     file = job_name + ".sh"
@@ -23,7 +36,15 @@ def create_individual_sbatch(job_num, start_npt, distance, total_runs_per_distan
         f.write(cmd)
     return file
 
-def create_dependency_sh(individual_shs):
+def create_dependency_sh(individual_shs: list) -> None:
+    """
+    Creates the master bash submit script for all jobs in system.
+
+    Input:
+        individual_sh (list): list containing all job submit scripts to be submitted
+
+    Output: None
+    """
     length = len(individual_shs)
     f = open("run.sh", "w")
     f.write("#!/bin/bash\n\n")
